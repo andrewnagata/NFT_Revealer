208fe5f3af6a4beb691c4c089e545c29407956ae
andrew <andrew@rollyk.com>
2022-05-01 10:29:22 -0700

    fix bucket name

--- api/app/s3_adapter.py
+++ api/app/s3_adapter.py
@@ -3,19 +3,17 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 KEY = os.getenv('AWS_ACCESS_KEY')
 SECRET = os.getenv('AWS_SECRET_KEY')
-S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
 
 s3 = boto3.client('s3',
          aws_access_key_id=KEY,
          aws_secret_access_key=SECRET)
 
 def upload_image(path_to_file: str, filename: str, folder: str = None):
-    print("USING BUCKET")
-    print(S3_BUCKET_NAME)
-    key = filename
+    load_dotenv()
+    S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
+    key = filename  
     if folder != None:
         key = folder + "/" + filename
 
@@ -27,6 +25,8 @@ def upload_image(path_to_file: str, filename: str, folder: str = None):
     )
 
 def upload_metadata(path_to_file: str, filename: str, folder: str = None):
+    load_dotenv()
+    S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
     key = folder + "/" + filename
     s3.upload_file(
         path_to_file,
