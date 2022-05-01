b46ca30131ad97849209ea3c34a88841ce1f7690
andrew <andrew@rollyk.com>
2022-05-01 13:38:56 -0700

    multiple id upload

--- api/app/revealer.py
+++ api/app/revealer.py
@@ -19,8 +19,10 @@ def new_event(event: Event):
     if event.contractCall.methodName != "publicMint":
         return
 
+    count = event.contractCall.params["count"]
     global index
-    index += 1
+    start = index
+    index += count
 
     # PATH be different on prod server - beware
     # should check to see if dev or prod
@@ -28,16 +30,18 @@ def new_event(event: Event):
     root_image_path = ROOT_DIR + "/images"
     root_meta_path = ROOT_DIR + "/metadata"
 
-    # Get files for index
-    image_file = str(index) + ".jpg"
-    path_to_image = os.path.join(root_image_path, image_file)
-    meta_file = str(index)
-    path_to_meta = os.path.join(root_meta_path, meta_file)
+    for i in range(0,count):
+        # Get files for index
+        next = start + i
+        image_file = str(next) + ".jpg"
+        path_to_image = os.path.join(root_image_path, image_file)
+        meta_file = str(next)
+        path_to_meta = os.path.join(root_meta_path, meta_file)
 
-    upload_image(path_to_image, image_file)
-    print(f"Uploaded image: {image_file}")
+        upload_image(path_to_image, image_file)
+        print(f"Uploaded image: {image_file}")
 
-    upload_metadata(path_to_meta, meta_file, "meta")
-    print(f"Uploaded metadata: {meta_file}")
+        upload_metadata(path_to_meta, meta_file, "meta")
+        print(f"Uploaded metadata: {meta_file}")
 
-    print(f"Upload complete for token: {index}")
+        print(f"Upload complete for token: {next}")
