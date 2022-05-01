cabb97b0d9a2fb6d4ccc5c2e809e16aa896b6afc
andrew <andrew@rollyk.com>
2022-05-01 11:40:20 -0700

    dotenv fixes

--- api/app/s3_adapter.py
+++ api/app/s3_adapter.py
@@ -2,35 +2,34 @@ import boto3
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(verbose=True)
+S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
 KEY = os.getenv('AWS_ACCESS_KEY')
 SECRET = os.getenv('AWS_SECRET_KEY')
 
 s3 = boto3.client('s3',
-         aws_access_key_id=KEY,
-         aws_secret_access_key=SECRET)
+        aws_access_key_id=KEY,
+        aws_secret_access_key=SECRET)
 
 def upload_image(path_to_file: str, filename: str, folder: str = None):
-    load_dotenv()
-    S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
+
     key = filename  
     if folder != None:
         key = folder + "/" + filename
 
     s3.upload_file(
         path_to_file,
-        'pizzapalsmedia',
+        S3_BUCKET_NAME,
         Key=key,
         ExtraArgs={ "ContentType": "image/jpeg" }
     )
 
 def upload_metadata(path_to_file: str, filename: str, folder: str = None):
-    load_dotenv()
-    S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
     key = folder + "/" + filename
+
     s3.upload_file(
         path_to_file,
-        'pizzapalsmedia',
+        S3_BUCKET_NAME,
         Key=key,
         ExtraArgs={ "ContentType": "json" }
     )
